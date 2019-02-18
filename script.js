0066730a3923b638a27f18672255e85aaa2051a7
Nathan Fowler <luxurysandbox@gmail.com>
2019-02-18 17:14:35 -0500

    j

--- script.js
+++ script.js
@@ -1,30 +1,51 @@
 'use strict';
 
 //fetch user info
-function getUser() {
-  console.log(`getUser ran`);
-};
-
-//create query string
-function createQueryString() {
-  console.log(`createQueryString ran`);
+function getUser(user) {
+  const url = `https://api.github.com/users/${user}/repos`
+  const options = {
+    headers: new Headers({
+      "Accept": "application/vnd.github.v3+json"
+    })
+  };
+  fetch(url, options)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error (response.statusText);
+  })
+  .then(responseJson => displayResults(responseJson, user))
+  .catch(err => {
+    $('#js_error_message').text(`Something went wrong: ${err.message}`);
+  });
 };
 
 //handle form
 function handleForm() {
-  console.log(`handleForm ran`);
+  $('#js_form').submit(function(e) {
+    e.preventDefault();
+    const user =  $('#js_username').val();
+    getUser(user);
+  });
 };
 
 //display user info
-function displayResults() {
-  console.log(`displayResults ran`);
+function displayResults(responseJson, user) {
+  console.log(responseJson);
+  const header = `<h2 class="username">${user}</h2>`
+  const name = responseJson.forEach(element => element.name);
+  console.log(name);
+  //empty any previous search
+  $('.results').empty();
+  $('.results').prepend(header);
+  $('.results').append(results);
+  //iterate through the user array
+  $('.results').toggleClass('hidden');
 };
 
 function handlePage() {
-  getUser();
-  createQueryString();
   handleForm();
-  displayResults();
 };
 
 $(handlePage());
