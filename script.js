7be3c410ab4e6a88446b27668f261b815b255865
Nathan Fowler <luxurysandbox@gmail.com>
2019-02-18 15:24:37 -0500

    added framework

---
+++ script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+//fetch user info
+function getUser() {
+  console.log(`getUser ran`);
+};
+
+//create query string
+function createQueryString() {
+  console.log(`createQueryString ran`);
+};
+
+//handle form
+function handleForm() {
+  console.log(`handleForm ran`);
+};
+
+//display user info
+function displayResults() {
+  console.log(`displayResults ran`);
+};
+
